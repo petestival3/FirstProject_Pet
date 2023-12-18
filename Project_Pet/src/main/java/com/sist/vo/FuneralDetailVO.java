fa5a2e577960456673f1a68669be28feee3a68bd
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 <officeight@naver.com>
2023-12-18 18:25:21 +0900

    ㅈㅇㄴ 12/18 18:25

---
+++ Project_Pet/src/main/java/com/sist/vo/FuneralDetailVO.java
@@ -0,0 +1,30 @@
+package com.sist.vo;
+public class FuneralDetailVO {
+	private int mno;
+	private String infoimage, infoh3, infop;
+	
+	public int getMno() {
+		return mno;
+	}
+	public void setMno(int mno) {
+		this.mno = mno;
+	}
+	public String getInfoimage() {
+		return infoimage;
+	}
+	public void setInfoimage(String infoimage) {
+		this.infoimage = infoimage;
+	}
+	public String getInfoh3() {
+		return infoh3;
+	}
+	public void setInfoh3(String infoh3) {
+		this.infoh3 = infoh3;
+	}
+	public String getInfop() {
+		return infop;
+	}
+	public void setInfop(String infop) {
+		this.infop = infop;
+	}
+}
