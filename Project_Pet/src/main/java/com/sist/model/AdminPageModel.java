09fa0c82091841b450277da2246b6788bfb6559a
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 <officeight@naver.com>
2023-12-22 11:03:16 +0900

    ㅈㅇㄴ 12/22 11:03

--- Project_Pet/src/main/java/com/sist/model/AdminPageModel.java
+++ Project_Pet/src/main/java/com/sist/model/AdminPageModel.java
@@ -1,5 +1,16 @@
 package com.sist.model;
 
-public class AdminPageModel {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import com.sist.controller.RequestMapping;
+
+public class AdminPageModel {
+	@RequestMapping("adminPage/ad_reserve.do")
+	public String admin_reserve(HttpServletRequest request, HttpServletResponse response)
+	{
+	      // 3. 결과값 모아서 request에 저장
+		  request.setAttribute("main_jsp", "../adminPage/ad_reserve.jsp");
+		  return "../main/main.jsp";
+	}
 }
--- Project_Pet/src/main/webapp/adminPage/ad_reserve.jsp
+++ Project_Pet/src/main/webapp/adminPage/ad_reserve.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+<h1>관리자 예약</h1>
 </body>
 </html>
