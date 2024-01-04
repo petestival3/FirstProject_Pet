79d251b257317cd08f24c50339f4b31f0108ef3e
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 <officeight@naver.com>
2024-01-04 17:33:16 +0900

    una 0104

--- Project_Pet/src/main/java/com/sist/model/JJimModel.java
+++ Project_Pet/src/main/java/com/sist/model/JJimModel.java
@@ -1,5 +1,18 @@
 package com.sist.model;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sist.controller.RequestMapping;
+
 public class JJimModel {
 
+	@RequestMapping("mypage/my_JJim.do")
+	public String resList(HttpServletRequest request, HttpServletResponse response)
+	{
+		request.setAttribute("mypage_jsp", "../mypage/my_JJim.jsp");
+		request.setAttribute("main_jsp", "../mypage/mypage.jsp");
+		
+		return "../main/main.jsp";
+	}
 }
--- Project_Pet/src/main/java/com/sist/model/LikeModel.java
+++ Project_Pet/src/main/java/com/sist/model/LikeModel.java
@@ -7,12 +7,5 @@ import com.sist.controller.RequestMapping;
 
 public class LikeModel {
 	
-	@RequestMapping("mypage/my_like.do")
-	public String resList(HttpServletRequest request, HttpServletResponse response)
-	{
-		request.setAttribute("mypage_jsp", "../mypage/my_like.jsp");
-		request.setAttribute("main_jsp", "../mypage/mypage.jsp");
-		
-		return "../main/main.jsp";
-	}
+
 }
