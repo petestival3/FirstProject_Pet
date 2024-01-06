842390450e43d3118712eab6dd8eb92683511c96
YYYYYYna <officeight@naver.com>
2024-01-06 15:03:45 +0900

    una0106

--- Project_Pet/src/main/java/com/sist/model/LikeModel.java
+++ Project_Pet/src/main/java/com/sist/model/LikeModel.java
@@ -2,10 +2,25 @@ package com.sist.model;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.sist.controller.RequestMapping;
+import com.sist.dao.*;
+import com.sist.vo.*;
 
 public class LikeModel {
-	
-
+	 @RequestMapping("product/like_insert.do")
+	   public String like_insert(HttpServletRequest request,
+			   HttpServletResponse response)
+	   {
+		   String pno=request.getParameter("pno");
+		   HttpSession session=request.getSession();
+		   String id=(String)session.getAttribute("id");
+		   LikeVO vo=new LikeVO();
+		   vo.setPno(Integer.parseInt(pno));
+		   vo.setId(id);
+		   LikeDAO dao=LikeDAO.newInstance();
+		   dao.LikeInsert(vo);
+		   return "redirect:../food/food_detail.do?pno="+pno;
+	   }
 }
--- Project_Pet/src/main/java/com/sist/vo/LikeVO.java
+++ Project_Pet/src/main/java/com/sist/vo/LikeVO.java
@@ -1,5 +1,44 @@
 package com.sist.vo;
+/*
+--<좋아요 테이블>
+CREATE TABLE GOODS_LIKE(
+ glno NUMBER,
+ id VARCHAR2(20) ,
+ pno NUMBER ,
+ CONSTRAINT gl_glno_pk PRIMARY KEY (glno),
+ CONSTRAINT gl_id_fk FOREIGN KEY(id) REFERENCES MEMBER(id),
+ CONSTRAINT gl_pno_fk FOREIGN KEY(pno) REFERENCES PRODUCT_DETAIL(pno)
+);
 
+--<좋아요 시퀀스>
+CREATE SEQUENCE gl_glno_seq 
+START WITH 1
+INCREMENT BY 1
+NOCYCLE
+NOCACHE;
+ */
 public class LikeVO {
-
+	private int glno,pno;
+	private String id;
+	
+	public int getGlno() {
+		return glno;
+	}
+	public void setGlno(int glno) {
+		this.glno = glno;
+	}
+	public int getPno() {
+		return pno;
+	}
+	public void setPno(int pno) {
+		this.pno = pno;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
 }
